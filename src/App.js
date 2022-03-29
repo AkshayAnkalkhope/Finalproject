d8c4487e404212329af7d3df2d9d1e4d41a621b0
AkshayAnkalkhope <96038720+AkshayAnkalkhope@users.noreply.github.com>
2022-03-29 22:18:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Adminadd from "./Views/Adminadd";
+function App() {
+  return (
+    <>
+      <Adminadd />
+    </>
+  );
+}
+
+export default App;
